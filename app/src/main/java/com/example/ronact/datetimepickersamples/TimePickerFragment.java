4bb98449382e7ab1d5fdb8c47b43cae69226941c
mhmdroni <muhamadroni9996@gmail.com>
2017-10-22 16:07:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ronact/datetimepickersamples/TimePickerFragment.java
@@ -0,0 +1,28 @@
+package com.example.ronact.datetimepickersamples;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import com.example.ronact.datetimepickersamples.MainActivity;
+
+import java.util.Calendar;
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+}
